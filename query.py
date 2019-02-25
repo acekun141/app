a325261a2f0a0c979e58e48d993b62d3d6e1318d
unknown <acekun141@gmail.com>
2019-02-25 11:14:54 +0700

    x

--- query.py
+++ query.py
@@ -1,6 +1,5 @@
 import cx_Oracle
 connect = cx_Oracle.connect('travis/travis@localhost:1521/XE')
 cursor = connect.cursor()
-result1 = cursor.execute('SELECT * FROM sys.tables').fetchall()
+result1 = cursor.execute('SELECT * FROM admin').fetchall()
 print(result1)
-print(result2)
