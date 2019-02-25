abc913a519327f2dc0aa7eb1c1e872a3b2b2ccd8
unknown <acekun141@gmail.com>
2019-02-25 11:34:35 +0700

    x

--- query.py
+++ query.py
@@ -1,5 +1,5 @@
 import cx_Oracle
 connect = cx_Oracle.connect('travis/travis@localhost:1521/XE')
 cursor = connect.cursor()
-result1 = cursor.execute('SELECT *').fetchall()
+result1 = cursor.execute('SELECT * FROM auth_user').fetchall()
 print(result1)
